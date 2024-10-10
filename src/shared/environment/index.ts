977b3f6e5f83ec0b51580186ce78fa7d5213d1e0
Alexsandro Santos <web_alexsandro@hotmail.com>
2024-10-09 21:26:29 -0300

    Update index.ts

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -15,5 +15,5 @@ export const Environment = {
   /**
    * Url base de consultado dos dados dessa aplicação
    */
-  URL_BASE: 'https://erpteste.conab.com.br/',
+  URL_BASE: 'https://erpteste.conab.com.br:7211/',
 };
