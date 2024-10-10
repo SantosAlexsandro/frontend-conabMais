edf727fb365635374cf3f2b2eb4c086889461cc4
SantosAlexsandro <web_alexsandro@hotmail.com>
2024-10-10 11:59:35 -0300

    Mock access

--- src/shared/services/api/auth/AuthService.ts
+++ src/shared/services/api/auth/AuthService.ts
@@ -9,11 +9,22 @@ const auth = async (email: string, password: string): Promise<IAuth | Error> => 
   try {
     console.log(email);
     console.log(password);
+    /*
     const { data } = await Api.post('/tokens/', { email, password } );
     console.log('data', data);
     if (data) {
       return data;
+    }*/
+
+    if (
+      (email === 'marcelo.pimentel@conab.com.br' && password === '123456') || 
+        (email === 'alexsandro.santos@conab.com.br' && password === '123456')
+    ) {
+      const data = { token: 'fake-jwt-token' };
+      console.log('Mock data:', data);
+      return data;
     }
+      
 
     return new Error('Erro no login.');
   } catch (error) {
