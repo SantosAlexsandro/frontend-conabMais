7a7f4e5c05925b63cdedc62b24ed9e3a14246c8a
SantosAlexsandro <web_alexsandro@hotmail.com>
2024-10-14 10:24:36 -0300

    Update CategoriasService.ts

--- src/shared/services/api/categoria/CategoriasService.ts
+++ src/shared/services/api/categoria/CategoriasService.ts
@@ -21,7 +21,8 @@ const getAll = async (
     const urlRelativa =
     // `/api/Regiao/RetrievePage?filter&order=&pageSize=${Environment.LIMITE_DE_LINHAS}&pageIndex=1`;
     //const urlRelativa = `/cidades?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&nome_like=${filter}&id_like=${id}`;
-    '/api/Categoria/RetrievePage?filter=&order&pageSize=100&pageIndex=1';
+    //'/api/Categoria/RetrievePage?filter=&order&pageSize=100&pageIndex=1';
+    'api/regioes?filter&page';
     const { data, headers } = await Api.get(urlRelativa);
     if (data) {
       return {
